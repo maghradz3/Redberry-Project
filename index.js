8b9e19e7d4e0084eee76294ecaa024b741f27830
maghradz3 <120563893+maghradz3@users.noreply.github.com>
2023-02-04 11:12:41 +0400

    Home Page
    I have created  a home Page

---
+++ index.js
@@ -0,0 +1,6 @@
+const resumeClick = document.querySelector(".resumeText");
+
+resumeClick.addEventListener("click", function () {
+  window.location.assign("contact.html");
+  console.log("button clicked");
+});
